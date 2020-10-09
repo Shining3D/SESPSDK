0e9209e55f7b23d2cf4367a7d9aa70c23f5b7c3a
jiarui <jrui9512@163.com>
2020-10-09 19:54:27 +0800

    add files

---
+++ fabu_UPDATE/restart_VID05E3_PID0620_REV2307.bat
@@ -0,0 +1,14 @@
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+cd /d %~dp0
+"%~dp0/devcon.exe" restart "USB\VID_05E3&PID_0620&REV_2307"
